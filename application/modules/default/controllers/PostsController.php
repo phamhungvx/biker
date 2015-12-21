d41dbbdd829e1927cd495066ede25b0baabf6c6d
QuocHung <phamhungvx@gmail.com>
2015-12-22 00:15:02 +0700

    posts controller

--- application/modules/default/controllers/PostsController.php
+++ application/modules/default/controllers/PostsController.php
@@ -4,12 +4,38 @@
 */
 class PostsController extends Zend_Controller_Action
 {
-	
+	protected $_modePost;
+	public function init(){
+		$this->_modePost = new Default_Model_Posts();
+	}
+
 	public function indexAction(){
-		$model = new Default_Model_Posts();
-		$data = $model->getAll();
-		$this->view->data = $data;
-		var_dump($data);
+		$arrParams = array();
+		// Default public post
+		$intLimit = 10;
+		$intPage = 1;
+		$intStatus = 1;
+
+		$data = $this->_request->getParams();
+
+		if( isset($data['page']) && $data['page'] != 0){
+			$intPage = $data['page'];
+		}
+		if( isset($data['status']) && $data['status'] != 0){
+			$intStatus = $data['status'];
+			$arrParams['status'] = $intStatus;
+		}
+		if( isset($data['author']) && $data['author'] != 0){
+			$arrParams['author'] = $data['author'];
+		}
+		if( isset($data['name']) && $data['name'] != ''){
+			$arrParams['name'] = $data['name'];
+		}
+
+
+		$arrPostData = $model->getAll($arrParams, $intLimit, $intPage);
+		$this->view->data = $arrPostData;
+		var_dump($arrPostData);
 		exit;
 	}
 }
--- application/modules/default/models/Posts.php
+++ application/modules/default/models/Posts.php
@@ -9,17 +9,33 @@ class Default_Model_Posts extends Zend_Db_Table_Abstract{
 	public function init(){
 		$this->_db = Zend_Registry::get('db');
 	}
-	public function getAll($arrFields, $arrWhere = array()){
-		if(empty($arrFields)){
-			$arrFields = '*'
-		}
-		$strSql = $this->_db->select($arrFields)
+
+	/**
+	 * Get All data
+	 *
+	 * @param 	$arrParams	Array params to search
+	 * @param  	$intLimut		row per page
+	 * @param 	$intPage		page request
+	 *
+	 * @return 	$arrResult
+	 */
+	public function getAll($arrParams, $intLimit = 10, $intPage = 1){
+
+		$strSelect = $this->_db->select()
 						->from('users');
-		if(!empty($arrWhere)){
-			$strSql = $strSql->where();
+
+		if( isset($arrParams['status']) && $arrParams['status'] != 0){
+			$strSelect = $strSelect->where('status = ?', $arrParams['status']);
 		}
-		$result = $this->_db->fetchAll($strSql);
-		return $result;
+		if( isset($arrParams['author']) && $arrParams['author'] != 0){
+			$strSelect = $strSelect->where('author = ?', $arrParams['author']);
+		}
+		if( isset($arrParams['name']) && $arrParams['name'] != ''){
+			$strSelect = $strSelect->where('title like %?%', $arrParams['author']);
+		}
+
+		$arrResult = $this->_db->fetchAll($strSelect);
+		return $arrResult;
 	}
 
 	public function count(){
